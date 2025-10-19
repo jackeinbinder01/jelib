35aba5dfb8493a35a18353c9d669f3d17bf6f7ea
Jack Einbinder <jackeinbinder01@gmail.com>
2025-10-19 13:09:00 -0400

    refactor

--- src/__init__.py
+++
@@ -1,8 +0,0 @@
-from .core.log import logger, setup_logging
-from .core.status import Status
-
-__all__ = [
-    "logger",
-    "setup_logging",
-    "Status"
-]
---
+++ src/jelib/__init__.py
@@ -0,0 +1,25 @@
+import logging
+
+from .core.log import logger, setup_logging
+from .core.status import Status
+
+
+def init_project(debug: bool = False, logfile: str | None = None) -> None:
+    """
+    Initializes logging and core configuration for projects using jelib.
+
+    Parameters:
+        debug (bool): If True, sets logging level to DEBUG, else INFO.
+        logfile (str | None): If provided, logs will be written to the file.
+    """
+    level = logging.DEBUG if debug else logging.INFO
+    setup_logging(level=level, filename=logfile)
+    logger.info("Project iitialized using jelib.")
+
+
+__all__ = [
+    "init_project",
+    "setup_logging",
+    "logger",
+    "Status",
+]
